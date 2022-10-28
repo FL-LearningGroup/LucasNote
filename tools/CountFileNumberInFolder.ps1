547b76b40a909ea7465eb9d5736f883ce0d9a8a4
Lucas Yao <v-diya@microsoft.com>
2022-10-28 20:16:21 +0800

    record

---
+++ tools/CountFileNumberInFolder.ps1
@@ -0,0 +1,84 @@
+<#
+.SYNOPSIS
+Count the number of files containing strings in specifiy folder.
+#>
+class FolderParallelSync {
+    [int]$Id
+    [string]$ModuleName
+    [string]$FolderPath
+}
+
+try {
+    $scriptPath = $PSScriptRoot
+    $topFolderPath = "C:\Users\v-diya\repository\azure-powershell\Main\azure-powershell\src"
+    Set-Location -Path $repoPath 
+    $moduleParallelDataSet = @();
+    $id = 0;
+    foreach($moduleFolderPath in (Get-ChildItem -Path $topFolderPath -Directory).FullName) {
+        $moduleParallel = [FolderParallelSync]::new()
+        $moduleParallel.Id = $id++
+        $moduleParallel.ModuleName = $moduleFolderPath.Split('\')[-1]
+        $moduleParallelDataSet += $moduleParallel
+    }
+    # Create a hashtable for process.
+    # Keys should be ID's of the processes
+    $origin = @{}
+    $moduleSummaryOrigin = @{}
+    foreach($dataSet in $moduleParallelDataSet) {
+        $origin.($dataSet.Id) = @{}
+        $moduleSummaryOrigin.($dataSet.ModuleName) = 0
+    }
+    # Create synced hashtable
+    $sync = [System.Collections.Hashtable]::Synchronized($origin);
+    $moduleSummarySync = [System.Collections.Hashtable]::Synchronized($moduleSummaryOrigin);
+    
+
+    $job = $moduleParallelDataSet | ForEach-Object -ThrottleLimit 5 -AsJob -Parallel {
+        $syncCopy = $using:sync
+        $moduleSummarySyncCopy = $using:moduleSummarySync
+        # Set property id of the syncCopy address point process.
+        $process = $syncCopy.$($PSItem.Id)
+        # Pass value of the id of the dataset to property id of the process.
+        $process.Id = $PSItem.Id
+        $process.Activity = "Id $($PSItem.Id) $($PSItem.ModuleName) starting"
+        $process.Status = "Processing"
+        
+        $filePathList = (Get-ChildItem -Path $PSItem.FolderPath -Recurse -File).FullName
+        $filePathCount = $filePathList.Length
+        for($fileNumber = 0;  $fileNumber -lt $filePathCount; $fileNumber++) {
+            $process.Status = "Handling $($fileNumber + 1)/$filePathCount"
+            $process.PercentComplete = (($fileNumber / $filePathCount) * 100)
+
+            if(($filePathList[$fileNumber] | Get-Content | Out-String) -match 'https://docs.microsoft.com/powershell') {
+                ($moduleSummarySyncCopy.$($PSItem.ModuleName))++
+            }
+        }
+        $process.Completed = $true
+    }
+    while($job.State -eq 'Running')
+    {
+        $sync.Keys | Foreach-Object {
+            # If key is not defined, ignore
+            if(![string]::IsNullOrEmpty($sync.$_.keys))
+            {
+                # Create parameter hashtable to splat
+                $param = $sync.$_
+    
+                # Execute Write-Progress
+                Write-Progress @param
+            }
+        }
+    
+        # Wait to refresh to not overload gui
+        Start-Sleep -Seconds 0.1
+    }
+
+    Write-Host -ForegroundColor Green "Summary result"
+    $moduleSummarySync
+
+} catch {
+    Write-Error "An error occurred"
+    Write-Error ($_ | Out-String)
+} finally {
+    Set-Location $scriptPath
+}
---
+++ tools/GenerateCmdletTestcases.ps1
@@ -0,0 +1,82 @@
+# Design
+# 1. Geneterate test cases of the cmdlet base on the cmdlet doc.
+# 2. Get variations and examples of cmdlets. They must be guaranteed to be in the same order.
+# 4. Parse example, include parameter value.
+class CmdletItem {
+    [string]$Variant
+    [string]$TestItemScript
+    [string[]]$Examples
+    [string[]]$TestCases
+}
+
+class Cmdlet {
+    [string]$Name
+    [string]$TestScript
+    [CmdletItem[]]$Items
+}
+
+$cmdletCollection = @()
+
+$variableConfigurations = @{
+    Tenant = '$env.Tenant';
+    Subscription = '$env.SubscriptionId';
+    ResourceGroupName = '$env.resourceGroupName';
+    Location = '$env.location';
+}
+
+$testScriptFormat = "Describe 'cmdlet' {`ntest`n}"
+$testCaseScriptFormat = "`tIt 'variant' {`n`t`t{`ntestcases`n`t`t} | Should -Not -Throw`n`t}"
+
+foreach($file in (Get-ChildItem -Path ./docs -Exclude 'Az.*','README.md')) {
+    $cmdlet = [Cmdlet]::new()
+    $cmdlet.Name = $file.Name.Split('.')[0]
+    $cmdletDocText = (Get-Content -Path $file.FullName) | Out-String
+    $cmdletSytanxs = $cmdletDocText.Substring($cmdletDocText.IndexOf('## SYNTAX'), $cmdletDocText.IndexOf('## DESCRIPTION') - $cmdletDocText.IndexOf('## SYNTAX')).Split([System.Environment]::NewLine,[System.StringSplitOptions]::RemoveEmptyEntries)
+    $cmdletExamples = $cmdletDocText.Substring($cmdletDocText.IndexOf('## EXAMPLES'), $cmdletDocText.IndexOf('## PARAMETERS') - $cmdletDocText.IndexOf('## EXAMPLES')).Split([System.Environment]::NewLine,[System.StringSplitOptions]::RemoveEmptyEntries)
+    # Get variant for the cmdlet sytanx
+    foreach($sytanx in $cmdletSytanxs) {
+        if ($sytanx -match '###') {
+            $cmdletItem = [CmdletItem]::new()
+            $cmdletItem.Variant = $sytanx.Split(' ')[1]
+            $cmdlet.Items += $cmdletItem
+        }
+    }
+    # variant index for keep vaiant and example keep in same order.
+    $variantIndex = 0
+    for($index=0; $index -lt $cmdletExamples.Length; $index++) {
+        if($cmdletExamples[$index] -match '```powershell') {
+            $index++ # Skip ```powershell line
+            do {
+                $cmdlet.Items[$variantIndex].Examples += $cmdletExamples[$index]
+            }until ($cmdletExamples[++$index] -match '```')
+            $variantIndex++
+            if ($variantIndex -ge $cmdlet.Items.Length) {
+                break
+            }
+        }
+    }
+
+    $cmdletCollection += $cmdlet
+}
+
+# Convert example to test case and generate test script
+foreach($cmdlet in $cmdletCollection) {
+    foreach($item in $cmdlet.Items) {
+        for($index = 0; $index -lt $item.Examples.Length; $index++) {
+            $item.TestCases += $item.Examples[$index] # TODO: Later add convert logicl.
+        }
+        # Add three Tab(`t`t`t) when generate test item script for file format
+        $item.TestItemScript = $testCaseScriptFormat.Replace('variant', $item.Variant).Replace('testcases', ($item.Examples | ForEach-Object {"`t`t`t$_"}) -join "$([System.Environment]::NewLine)")
+    }
+
+    $cmdlet.TestScript = $testScriptFormat.Replace('cmdlet', $cmdlet.Name).Replace('test', ($cmdlet.Items.TestItemScript -join "$([System.Environment]::NewLine)$([System.Environment]::NewLine)"))
+}
+
+# Insert cmdlet test to cmdlet test ps1 file.
+foreach($cmdlet in $cmdletCollection) {
+    $cmdletTestPs1File = "./test/$($cmdlet.Name).Tests.ps1"
+    $cmdletTestPs1Content = Get-Content -Path $cmdletTestPs1File  | Out-String
+    $cmdletTestPs1Content -replace "Describe((.|`n)*)", $cmdlet.TestScript | Set-Content -Path $cmdletTestPs1File
+}
+
+$cmdletCollection.Items | Format-Table -AutoSize
